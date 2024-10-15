a2d6a5d7f8deb1adce8ef757ed4a3ea633b1d777
sjorgedev <sandovl06roque@gmail.com>
2024-10-14 21:06:25 -0700

    fix: vercel adapter error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'
-import vercel from '@astrojs/vercel'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: vercel()
+    adapter: vercel(),
 });
